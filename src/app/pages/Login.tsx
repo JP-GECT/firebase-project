79bce112399ce28e450f92d9375e5c22a204518d
JP-GECT <100013491+JP-GECT@users.noreply.github.com>
2023-05-25 12:30:19 +0530

    update login.tsx

--- src/app/pages/Login.tsx
+++ src/app/pages/Login.tsx
@@ -1,11 +1,16 @@
+"use client"
+import { useState } from "react"
 
 const Login = () =>{
+    const [error, setError] = useState(false);
     return(
         <div className='h-screen flex items-center justify-center'>
             <form className=' flex flex-col border-slate-500 border-solid border-3 items-center justify-center'>
                 <input className="w-[200px] h-[30px] m-[10px] border-slate-500 border-solid border-2 rounded-lg " type="email" placeholder="email"></input>
                 <input className="w-[200px] h-[30px] m-[10px] border-slate-500 border-solid border-2 rounded-lg " type="password" placeholder="password"></input>
-                <button type="submit" className='w-[200px] h-[40px] bg-slate-200 rounded-sm hover:bg-slate-300'>Login</button>
+                <button type="submit" className='w-[200px] h-[40px] bg-slate-200 rounded-sm hover:bg-slate-300 font-bold'>Login</button>
+
+                {error && <span className='text-[12px] text-red-500 mt-[10px]'>Wrong email or password</span>}
             </form>
         </div>
     )
